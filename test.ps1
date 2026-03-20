8cd5966b3764572ca59aff6e1653b728cc632370
olympusmons5143 <justin.franks@live.com>
2026-03-20 08:10:02 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,2 +1,4 @@
 Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.MessageBox]::Show("This is a test.", "Test", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+#[System.Windows.Forms.MessageBox]::Show("This is a test.", "Test", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+$info = "User: $(whoami)`nHost: $(hostname)`nIP: $((Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceAlias -notlike '*Loopback*'} | Select-Object -First 1).IPAddress)"
+[System.Windows.Forms.MessageBox]::Show($info, "System Info Captured", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Warning)
